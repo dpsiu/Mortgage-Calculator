02175e7de8871bba064e5a027f0627b9f910d8f6
dpsiu <denversiu24@gmail.com>
2023-10-09 14:24:34 -0500

    Add styles
    - add input symbols
    - round total
    - total base styling

--- components/MortgageCalculatorInput.jsx
+++ components/MortgageCalculatorInput.jsx
@@ -6,10 +6,10 @@ export function MortgageCalculatorInput() {
   const [count, setCount] = useState(0);
 
   const initialInputState = {
-    homeprice: "",
-    downpayment: "",
-    loanterm: "",
-    interestrate: "",
+    homeprice: "425000",
+    downpayment: "85000",
+    loanterm: "30",
+    interestrate: "8",
     zipcode: "78701",
     propertytax: "",
     homeownerinsurance: "",
@@ -17,8 +17,6 @@ export function MortgageCalculatorInput() {
     hoafee: "",
     monthlyPayment: "",
   };
-
-  const calculateMortgage = () => {};
 
   const [mortgageInputs, setMortgageInputs] = useState(initialInputState);
 
@@ -30,26 +28,15 @@ export function MortgageCalculatorInput() {
   // L = Mortgage loan amount
   // C = Your mortgage interest rate
   // N = Number of monthly payments over
-
   const L = mortgageInputs.homeprice - mortgageInputs.downpayment;
   const C = mortgageInputs.interestrate / 12 / 100;
   const N = mortgageInputs.loanterm * 12;
-  const P = (L * (C * (1 + C) ** N)) / ((1 + C) ** N - 1);
+  const P = ((L * (C * (1 + C) ** N)) / ((1 + C) ** N - 1)).toFixed(2)
 
   useEffect(() => {
-    // console.log(mortgageInputs.homeprice);
-    // console.log(mortgageInputs.downpayment);
-    // console.log(mortgageInputs.loanterm);
-    // console.log(mortgageInputs.interestrate);
-
     setMortgageInputs({
       ...mortgageInputs,
       monthlyPayment: P,
-      // (mortgageInputs.homeprice - mortgageInputs.downpayment) *
-      // ((mortgageInputs.interestrate *
-      //   (1 + mortgageInputs.interestrate) ** (mortgageInputs.loanterm * 12)) /
-      //   (1 + mortgageInputs.interestrate) ** (mortgageInputs.loanterm * 12) -
-      //   1),
     });
   }, [
     mortgageInputs.homeprice,
@@ -66,7 +53,7 @@ export function MortgageCalculatorInput() {
 
   const handleValidInput = (e) => {
     console.log(e.key);
-    e.key ===
+    e.key ==
       [
         ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(e.key)
           ? true
@@ -79,36 +66,45 @@ export function MortgageCalculatorInput() {
     <>
       <div className="flex flex-col p-4 w-full">
         <div>
-          <div className="pb-4">
-            <p className="pb-2 w-full text-sm font-bold ">Home Price</p>
-            <input
-              className="py-2 px-2 border border-zinc-500 rounded-md hover:bg-blue-100/50 focus:border-blue-700 focus:outline-none w-full appearance-none"
-              type="text"
-              value={mortgageInputs.homeprice}
-              onKeyPress={(e) => handleValidInput(e)}
-              // Need to account for minimum $10,000 price. Need extra function? Or modification to current is possible?
-              onChange={(e) =>
-                setMortgageInputs({
-                  ...mortgageInputs,
-                  homeprice: e.target.value,
-                })
-              }
-            />
+          <div className="pb-4 relative flex flex-col">
+            <p className="pb-2 w-full text-sm font-bold">Home Price</p>
+            <div className="relative flex items-center">
+              <span className="absolute inset-y-0 left-0 pl-2 flex items-center">
+                $
+              </span>
+              <input
+                className="py-2 px-5 border border-zinc-500 rounded-md hover:bg-blue-100/50 focus:border-blue-700 focus:outline-none w-full appearance-none"
+                type="text"
+                value={mortgageInputs.homeprice}
+                onKeyPress={(e) => handleValidInput(e)}
+                onChange={(e) =>
+                  setMortgageInputs({
+                    ...mortgageInputs,
+                    homeprice: e.target.value,
+                  })
+                }
+              />
+            </div>
           </div>
-          <div className="pb-4">
+          <div className="pb-4 relative flex flex-col">
             <p className="pb-2 w-full text-sm font-semibold ">Down Payment</p>
-            <input
-              className="py-2 px-2 border border-zinc-500 rounded-md hover:bg-blue-100/50 focus:border-blue-700 focus:outline-none w-full appearance-none"
-              type="number"
-              value={mortgageInputs.downpayment}
-              onKeyPress={(e) => handleValidInput(e)}
-              onChange={(e) =>
-                setMortgageInputs({
-                  ...mortgageInputs,
-                  downpayment: e.target.value,
-                })
-              }
-            />
+            <div className="relative flex items-center">
+              <span className="absolute inset-y-0 left-0 pl-2 flex items-center">
+                $
+              </span>
+              <input
+                className="py-2 px-5 border border-zinc-500 rounded-md hover:bg-blue-100/50 focus:border-blue-700 focus:outline-none w-full appearance-none"
+                type="number"
+                value={mortgageInputs.downpayment}
+                onKeyPress={(e) => handleValidInput(e)}
+                onChange={(e) =>
+                  setMortgageInputs({
+                    ...mortgageInputs,
+                    downpayment: e.target.value,
+                  })
+                }
+              />
+            </div>
           </div>
           <div className="pb-4">
             <p className="pb-2 w-full text-sm font-semibold ">Loan Term</p>
@@ -125,20 +121,25 @@ export function MortgageCalculatorInput() {
               }
             />
           </div>
-          <div className="pb-4">
-            <p className="pb-2 w-full text-sm font-semibold ">Interest Rate</p>
-            <input
-              className="py-2 px-2 border border-zinc-500 rounded-md hover:bg-blue-100/50 focus:border-blue-700 focus:outline-none w-full appearance-none"
-              type="number"
-              value={mortgageInputs.interestrate}
-              onKeyPress={(e) => handleValidInput(e)}
-              onChange={(e) =>
-                setMortgageInputs({
-                  ...mortgageInputs,
-                  interestrate: e.target.value,
-                })
-              }
-            />
+          <div className="pb-4 relative flex flex-col">
+            <p className="pb-2 w-full text-sm font-bold">Interest Rate</p>
+            <div className="relative flex items-center">
+              <span className="absolute inset-y-0 right-0 pr-2 flex items-center">
+                %
+              </span>
+              <input
+                className="py-2 px-2 border border-zinc-500 rounded-md hover:bg-blue-100/50 focus:border-blue-700 focus:outline-none w-full appearance-none"
+                type="text"
+                value={mortgageInputs.interestrate}
+                onKeyPress={(e) => handleValidInput(e)}
+                onChange={(e) =>
+                  setMortgageInputs({
+                    ...mortgageInputs,
+                    interestrate: e.target.value,
+                  })
+                }
+              />
+            </div>
           </div>
           <div className="pb-4">
             <p className="pb-2 w-full text-sm font-semibold ">ZIP Code</p>
@@ -242,8 +243,8 @@ export function MortgageCalculatorInput() {
             </div>
           )}
         </div>
-        <div>
-          <h3>Total:{mortgageInputs.monthlyPayment}</h3>
+        <div className="relative flex items-center text-lg font-bold">
+          <h3>Total monthly payment = <span>$</span>{mortgageInputs.monthlyPayment}</h3>
         </div>
       </div>
     </>
